d309cc8fbcb34e0cc98387a67d14526d67a7648e
Vitor Brandao <vitorbrandaosilva@outlook.com>
2021-08-24 22:22:43 -0300

    Exercicios BlogPessoal - Data: 24/08/21

---
+++ blogPessoal/src/main/java/org/generation/blogPessoal/model/Postagem.java
@@ -0,0 +1,68 @@
+package org.generation.blogPessoal.model;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+
+
+@Entity
+@Table(name = "postagens")
+public class Postagem {
+	@Id 
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private  long id;
+	
+	@NotBlank
+	@Size(min = 5, max = 100)
+	private String titulo;
+	
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date data = new java.sql.Date(System.currentTimeMillis());
+
+	@NotBlank
+	private String texto;
+		
+	
+	public String getTexto() {
+		return texto;
+	}
+
+	public void setTexto(String texto) {
+		this.texto = texto;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public Date getData() {
+		return data;
+	}
+
+	public void setData(Date data) {
+		this.data = data;
+	}
+	
+	
+}
